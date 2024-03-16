168d4a27423b3ff2e69ff580029ebf03a91bd421
devahm3d <162648583+devahm3d@users.noreply.github.com>
2024-03-16 15:06:27 +0200

    Update __init__.py

--- FallenMusic/__init__.py
+++ FallenMusic/__init__.py
@@ -92,8 +92,8 @@ async def fallen_startup():
     ASS_USERNAME = getme2.username
     ASS_MENTION = getme2.mention
     try:
-        await app2.join_chat("WX_PM")
-        await app2.join_chat("T7_AU")
+        await app2.join_chat("R1_O9")
+        await app2.join_chat("Ql_ll0")
     except:
         pass
 
